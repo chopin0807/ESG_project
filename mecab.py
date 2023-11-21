3492458c1cfb289df698ccc7eb29ded4b3d96530
unknown <chopin0807@naver.com>
2023-11-21 13:48:37 +0900

    파일 경로설정에 대한 코드 수정

--- mecab.py
+++ mecab.py
@@ -7,12 +7,12 @@ from collections import Counter
 
 warnings.filterwarnings(action = 'ignore')
 # 대상 기사데이터 파일 목록
-path = "./기사데이터"
+path = "./기사데이터(우리은행)/"
 file_list = os.listdir(path)
 data = pd.DataFrame({"기사날짜":[],"기사제목":[],"네이버 뉴스 url":[],"기사내용":[]})
 
 for i in file_list:
-    df = pd.read_csv("./기사데이터/" + i)
+    df = pd.read_csv(path + i)
     data = pd.concat([data, df], ignore_index = True)
 
 # 네이버 기사가 없거나, 스포츠 연예 기사 등 구조가 다른 기사 삭제

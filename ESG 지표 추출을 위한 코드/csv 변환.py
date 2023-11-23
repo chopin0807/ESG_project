a7c1d009aa33d50f31fc41117b7faf00d4c0acf0
bgm3167 <rhksals2001@gmail.com>
2023-11-23 11:22:51 +0900

    감정분석

---
+++ ESG 지표 추출을 위한 코드/csv 변환.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+# CSV 파일이 있는 폴더
+folder_path = 'C:/Users/rhksa/OneDrive/바탕 화면/ESG_project/기사데이터/우리은행/'
+
+# 폴더 내의 모든 CSV 파일 목록을 가져옵니다.
+csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+# 모든 CSV 파일을 DataFrame으로 읽어서 리스트에 저장합니다.
+dataframes = [pd.read_csv(os.path.join(folder_path, file)) for file in csv_files]
+
+# 모든 데이터프레임을 하나로 합칩니다.
+combined_csv = pd.concat(dataframes, ignore_index=True)
+
+combined_csv.to_csv('C:/Users/rhksa/OneDrive/바탕 화면/ESG_project/기사데이터/우리은행/combined_file.csv', index=False)
+

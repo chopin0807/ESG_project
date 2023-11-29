89f108e9f4bd48b75993e3053e667da63c52f8fd
unknown <chopin0807@naver.com>
2023-11-29 11:15:44 +0900

    streamlit의 시각화 라이브러리를 이용하여 일부 시각화 적용

--- 웹 서비스/web_service.py
+++ 웹 서비스/web_service.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 import os
 import pandas as pd
+import plotly.express as px
 
 # 터미널에서 실행(이 파이썬 파일에 속해 있는 디렉터리로 이동해서) -> streamlit run web_service.py
 
@@ -25,6 +26,24 @@ avg_S = round(df_S.mean(), 1)
 df_G = df[df['구분(ESG)'] == "G"]['점수']
 # G에 대한 평균
 avg_G = round(df_G.mean(), 1)
+# E와 관련된 뉴스
+E_news = df[df['구분(ESG)'] == 'E'].sort_values(by = '점수', ascending = False)
+# S와 관련된 뉴스
+S_news = df[df['구분(ESG)'] == 'S'].sort_values(by = '점수', ascending = False)
+# G와 관련된 뉴스
+G_news = df[df['구분(ESG)'] == 'G'].sort_values(by = '점수', ascending = False)
+
+# 시계열 데이터 시각화
+date_df = df[['날짜', '점수', '구분(ESG)']]
+date_df['date'] = date_df['날짜'].apply(lambda x:x.split(" ")[0])
+date_df['date'] = pd.to_datetime(date_df['date'])
+date_df = date_df[['date', '점수']]
+# 같은 날짜에 대한 점수를 평균화하여 처리
+date_avg_df = date_df.groupby('date').mean()
+date_avg_df = date_avg_df.reset_index()
+# date를 index로 지정
+# date_avg_df.index = date_avg_df['date']
+# date_avg_df.set_index('date', inplace=True)
 
 # 게이지 차트 그리기
 import plotly.graph_objects as go
@@ -85,15 +104,23 @@ if choose == "ESG 소개":
         st.write('ESG관련 논문 및 ISO자료로 부터 ESG요소별 키워드 추출 => 기사데이터 추출 => 기사데이터에서 ESG 요소 판단 => 기사데이터에서 긍/부정 판단....')
 
 elif choose == "ESG 서비스":
-    col1, col2, col3, col4 = st.columns([10, 5, 5, 5])
-    with col1:
-        st.image("전체.png")
-    with col2:
-        st.image("E(환경)분석.png")
-    with col3:
-        st.image("S(사회)분석.png")
-    with col4:
-        st.image("G(지배)분석.png")
+    tab1, tab2, tab3 = st.tabs(["ESG 전체 지표", "ESG각 요소별 평가지표 및 관련기사", "기간별 ESG지표 시계열 차트"])
+    with tab1:
+        pie = px.pie(values = [len(E_news), len(S_news), len(G_news)], names = ["Environmental", "Social", "Governance"])
+        st.write(pie)
+        bar = px.bar(x = ["Environmental", "Social", "Governance"], y = [len(E_news), len(S_news), len(G_news)])
+        st.write(bar)
+    with tab2:
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image("E(환경)분석.png")
+        with col2:
+            st.image("S(사회)분석.png")
+        with col3:
+            st.image("G(지배)분석.png")
+    with tab3:
+        line = px.line(data_frame= date_avg_df, x="date", y="점수")
+        st.write(line)
 
 elif choose == "참고자료":
     st.header("ESG 관련 논문 자료 및 ISO 표준 관련 참고자료")
